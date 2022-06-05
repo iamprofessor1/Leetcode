a17c5becb0cf7c1908ad7a6bd5aba7c439a440dd
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-06-06 00:12:47 +0530

    Time: 24 ms (48.77%), Space: 10.9 MB (29.82%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       vector<pair<int,int>>vp;
-        for(int i = 0 ; i< nums.size();i++){
-            vp.push_back(make_pair(nums[i],i));
-        }
-        sort(vp.begin(),vp.end());
-        int i = 0 ;
-        int j = nums.size()-1;
+       map<int,int>m;
         vector<int>ans;
-        while(i<j){
-            int fi = vp[i].first;
-            int se = vp[j].first;
-            if(fi + se == target){
-                ans.push_back(vp[i].second);
-                ans.push_back(vp[j].second);
-                break;
-            }
-            else if(fi + se > target){
-                j--;
+        for(int i = 0 ; i< nums.size() ; i++){
+            if(m.find(target-nums[i])==m.end()){
+//             insert element if not present 
+                m[nums[i]] = i;
             }
             else{
-                i++;
+//                 if target -  nums[i] already present then print their corressponding index
+                ans.push_back(i);
+                ans.push_back(m[target-nums[i]]);
+                break;
             }
         }
         return ans;
