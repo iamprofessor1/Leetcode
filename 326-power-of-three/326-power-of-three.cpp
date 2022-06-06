bdca38046c6961edaefb0831f250fa9e09f36994
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-06-06 17:23:47 +0530

    Time: 45 ms (15.34%), Space: 6 MB (28.84%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
-    bool log3(int n){
-        if(n%3 != 0 && n !=1){
-            return false;
-        }
-        if(n==1){
-            return true;
-        }
-        bool small_ans = log3(n/3);
-        return small_ans;
-        
-    }
+
     bool isPowerOfThree(int n) {
-        return n>0 and log3(n);
+        return n>0  and ((1162261467 % n) == 0);
     }
 };
