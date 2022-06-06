eef8fcdd487d5ccfa03a46329cb2d46ee69a48a3
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-06-06 18:24:05 +0530

    Time: 6 ms (17.69%), Space: 5.9 MB (30.15%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -5,18 +5,14 @@ public:
         int mini = min(num1,num2);
         int cnt=0;
         while(maxi !=0 &&  mini !=0){
-            int temp=maxi-mini;
-            maxi = max(temp,mini);
-            mini = min(temp,mini);
-            cnt++;
-            // cout<<maxi<<" "<<mini<<endl;
+            cnt += maxi/mini;
+            maxi = maxi%mini;
+            swap(maxi,mini);
+            
         }
         return cnt;
     }
 };
 
-
-// 3 2
-// 1 2
 
 
