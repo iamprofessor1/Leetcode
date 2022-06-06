a80b534244d36672456aad512b095de8a1d98a97
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-06-06 22:16:19 +0530

    Time: 10 ms (22.15%), Space: 6.9 MB (10.73%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -3,18 +3,18 @@ class Solution {
 public:
     
     bool reorderedPowerOf2(int n) {
-        map<char,long long >m1;
+        unordered_map<char,long long >m1;
         int count =0;
         string s= to_string(n);
         for(auto x : s){
             m1[x]++;
             count++;
         }
-        for(auto x : m1){
-            cout<<"fi"<<x.first<<" "<<"se"<<x.second<<endl;
-        }
+        
         vector<vector<string>>powerOftwo(11);
+        
         long long temp = 1;
+        
         while( temp <= 1e10 ){
             string s = to_string(temp);
             int t = (int)s.size();
@@ -25,13 +25,12 @@ public:
                 break;
             }
         }
-      
-        // poweroftwo[v.size()];
+        
         for(int i = 0 ; i<powerOftwo[count].size();i++){
             
             string s = powerOftwo[count][i];
             cout<<s<<endl;
-            map<char,long long>temp = m1;
+            unordered_map<char,long long>temp = m1;
             bool check = true;
             for(auto x :s){
                 if(temp[x] == 0){
@@ -44,7 +43,7 @@ public:
                 
             }
             if(check){
-                cout<<s<<" :"<<endl;
+                // cout<<s<<" :"<<endl;
                 return true;
             }
         }
@@ -52,9 +51,3 @@ public:
         return false;
     }
 };
-
-    // 1
-    //     10
-    //     100  6
-    //     1000
-    //     10000
