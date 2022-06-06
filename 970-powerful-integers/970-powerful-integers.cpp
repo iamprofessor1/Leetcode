34c8cdae58325e8086c8dcd26122ec34c133f095
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-06-07 00:25:39 +0530

    Time: 3 ms (50.00%), Space: 6.7 MB (54.31%) - LeetHub

---
+++ 970-powerful-integers/970-powerful-integers.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+   
+    
+    vector<int> powerfulIntegers(int x, int y, int bound) {
+        vector<int>ans;
+        set<int>s;
+        int mini = min(x,y);
+        int maxi = max(x,y);
+        x = mini;
+        y = maxi;
+        if(mini!=1&&maxi!=1){
+        for(long long i = 0 ; i <= ceil((log(bound)*1.0)/log(x));i++){
+            long  long a = pow(x,i);
+            for(long long j = 0 ; j <= ceil((log(bound)*1.0)/log(y));j++){
+               long long b = pow(y,j);
+                if(a+b<= bound && s.find(a+b)==s.end()){
+                    ans.push_back(a+b);
+                    s.insert(a+b);
+                }
+            }
+        
+        }}
+        else if (maxi!=1){
+            for(long long j = 0 ; j <= ceil((log(bound)*1.0)/log(y));j++){
+               long long b = pow(y,j);
+                if(1+b<= bound && s.find(1+b)==s.end()){
+                    ans.push_back(1+b);
+                    s.insert(1+b);
+                }
+            }
+        }
+        else{
+            if(2<= bound)
+            {
+            ans.push_back(2);
+            }
+        }
+        return ans;
+    }
+};
+
