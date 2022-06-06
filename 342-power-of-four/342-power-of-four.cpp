ad81599f56992e6fdca6e385aed3fed3259b9b04
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-06-06 17:52:37 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (88.41%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
+    bool log4(int n){
+        if((n<=0 )|| (n%4 != 0 && n!=1)){
+            return false;
+        }
+        else if(n==1){
+            return true;
+        }
+        bool small_ans = log4(n/4);
+        return small_ans;
+    }
     bool isPowerOfFour(int n) {
-        return (n>0) and (__builtin_popcount(n) ==1) && (__builtin_ctz(n)%2==0);
+        return log4(n);
     }
 };
-
-// 1 -> 1
-// 4 -> 100
-// 8-
