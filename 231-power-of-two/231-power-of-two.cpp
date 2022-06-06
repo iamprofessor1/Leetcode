f5c093bbcc6fcdc2f198115814b8e7fa2b9d0835
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-06-06 17:13:01 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (88.43%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         // return n >0  and floor(log2(n)) == ceil(log2(n)) ;
-        return (n>0) and ((n&(n-1))==0) ;
+        // return (n>0) and ((n&(n-1))==0) ;
+        return (n>0) and 2147483648 % n ==0;
     }
 };
