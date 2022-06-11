93dbd9728c31d763f76953ed9ff805de45cb5db3
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-06-12 02:53:28 +0530

    Time: 23 ms (42.41%), Space: 20.4 MB (17.51%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -4,17 +4,23 @@ public:
         int n =h.size();
         vector<int>lmax(n,0),rmax(n,0);
         lmax[0]= h[0];
+//         Water is always stored between the top height
+//         precalculating the leftmostmaxium value till that point including me
         for(int i = 1 ; i<n ;i++){
             lmax[i] = max(lmax[i-1],h[i]);
         }
+//         precalculating the rigthmostmaxium value till that point including me
         rmax[n-1] =h[n-1];
         for(int i = n-2 ;i>=0 ;i--){
             rmax[i] = max(rmax[i+1] ,h[i]);
         }
         //calculating total water
         int total_water = 0;
+//         No water is stored at 1st and 2nd block
         for(int i = 1 ; i<n-1;i++){
+//             itna water do baade baade pillar ke bicche mein hota 
             int choti_h = min(lmax[i],rmax[i]);
+//         itna uppar hai isisliye subtract kia hai
             total_water += choti_h - h[i];
         }
         return total_water;
