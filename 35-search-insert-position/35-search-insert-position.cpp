1c4185b0fc7830b425e9c730a226bcb8fbf32369
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-06-18 23:45:03 +0530

    Time: 4 ms (79.08%), Space: 9.6 MB (72.44%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+//         finding lower bound
+        int low = 0 , high = nums.size() -1;
+        int ans = nums.size();
+        while(low<=high){
+            int mid = low +(high - low)/2;
+            // cout<<low<<" "<<mid<<" "<<high<<endl;
+            if(nums[mid] == target){
+                ans = mid ;
+                return ans;
+            }
+           else if(nums[mid] < target){
+                ans = mid;
+                low = mid +1 ;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+};
