1b04dc33399d88595d9e9d8929a305375f5c8016
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-07-14 00:31:28 +0530

    Time: 473 ms (45.46%), Space: 23.1 MB (44.65%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        int ans = nums[0] + nums[1] + nums[2];
-        int closest = abs(target-ans);
-         for(int i = 0 ; i< nums.size() ; i++){
-             int start = i+1;
-             int end = nums.size()-1;
-             while(start<end){
-                int sum = nums[start]+ nums[end] + nums[i];
-                 if(sum == target){
-                     return sum;
-                 }
-                 else if(sum>target ){
-                     int temp =abs(target-sum);
-                     if(temp<closest){
-                         closest = temp;
-                         ans = sum;
-                     }
-                     end--;
-                 }
-                 else{
-                    int temp =abs(target-sum);
-                     if(temp<closest){
-                         closest = temp;
-                         ans = sum;
-                     }
-                     start++;
-                 }
-             }
-         }
+    int threeSumClosest(vector<int>& v, int target) {
+        sort(v.begin(),v.end());
+        int ans = v[0]+v[1]+v[2];
+        int min_diff = abs(ans-target);
+        for(int i = 0 ; i<v.size() ;i++){
+            int start = i+1;
+            int end = v.size()-1LL;
+            while(start<end){
+                int sum = v[start]+v[end]+v[i];
+                int diff = abs(sum - target) ;
+                if(sum==target){
+                    return sum;
+                }
+                else if(sum>target){
+                    if(min_diff > diff){
+                        min_diff = diff;
+                        ans = sum;
+                    }
+                    end--;
+                }
+                else{
+                    if(min_diff > diff){
+                        min_diff = diff;
+                        ans = sum;
+                    }
+                    start++;
+                }
+            }
+        }
         return ans;
     }
 };
